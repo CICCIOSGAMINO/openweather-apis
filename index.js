897365320aa8f1be91c842475050b0aa7f2b9b3a
Giskard Solutions <kamil4521@gmail.com>
2015-06-16 21:02:09 +0200

    Fixed JSHint mistakes

--- index.js
+++ index.js
@@ -6,10 +6,8 @@
     units : 'metric',
     lan : 'it',
     format : 'json',
-    APPID : null,
-
+    APPID : null
   };
-
 
   // main settings
   var http = require('http');
@@ -22,110 +20,109 @@
   // exports(set)  --------------------------------------------  exports(set)  ---------------------------------------------
   exports.setLang = function(lang){
     config.lan = lang.toLowerCase();
-  }
+  };
 
   exports.setCity = function(city){
     config.city = city.toLowerCase();
-  }
+  };
 
   exports.setUnits = function(units){
     config.units = units.toLowerCase();
-  }
+  };
 
   exports.setAPPID = function(appid){
     config.APPID = appid;
-  }
+  };
 
   // export(get)  ---------------------------------------------  exports(get)  ---------------------------------------------
   exports.getLang = function(){
     return config.lan;
-  }
+  };
 
   exports.getCity = function(){
     return config.city;
-  }
+  };
 
   exports.getUnits = function(){
     return config.units;
-  }
+  };
 
   exports.getFormat = function(){
     return config.format;
-  }
+  };
 
   exports.getError = function(callback){
      getErr(callback);
-  }
+  };
 
   exports.getAPPID = function(){
     return config.APPID;
-  }
+  };
 
 
   // get Response by field (satusCode, )
   exports.getResponseCode = function(callback){
     getResponseBF('statusCode', callback);
-  }
+  };
 
   // get temperature
   exports.getTemperature = function(callback){
     getTemp(callback);
-  }
+  };
 
   // get the atmospheric pressure
   exports.getPressure = function(callback){
     getPres(callback);
-  }
+  };
 
   exports.getHumidity = function(callback){
     getHum(callback);
-  }
+  };
 
   exports.getDescription = function(callback){
     getDesc(callback);
-  }
+  };
 
   exports.getAllWeather = function(callback){
     getData(callback);
-  }
+  };
 
   exports.getSmartJSON = function(callback){
     getSmart(callback);
-  }
+  };
 
   // active functions()  -------------------------------------  active functions()  --------------------------------------------
 
   function getErr(callback){
     // set new path to throw the http exception
     options.path = 'timetocrash';
-    var request = http.get(options, function(err,data){
+    http.get(options, function(err,data){
         return callback(err,data);
-    })
+    });
   }
 
   function getPres(callback){
     getData(function(err,jsonObj){
       return callback(err,jsonObj.main.pressure);
-    })
+    });
   }
 
   function getTemp(callback){
     getData(function(err,jsonObj){
       return callback(err,jsonObj.main.temp);
-    })
+    });
   }
 
   function getHum(callback){
     getData(function(err,jsonObj){
       return callback(err,jsonObj.main.humidity);
-    })
+    });
   }
 
   function getDesc(callback){
     getData(function(err,jsonObj){
-
       return callback(err, (jsonObj.weather)[0].description);
-    })
+    });
   }
 
   function getSmart(callback){
@@ -137,38 +134,32 @@
       smartJSON.description = (jsonObj.weather)[0].description;
       smartJSON.weathercode = ((jsonObj.weather[0]).id);
       return callback(err,smartJSON);
-    })
+    });
   }
 
   function getResponseBF(field,callback){
-    var req = http.get(options, function(res){
-      res.on('end', function(){
-      });
-			return callback(null, res[field]);
+    http.get(options, function(res){
+      res.on('end', function(){ /* Do nothing, meaby remove this line? */});
+      return callback(null, res[field]);
     });
   }
 
   function buildPath(){
-
     return '/data/2.5/weather?q=' + config.city + '&units=' + config.units + '&lang=' + config.lan + '&APPID=' + config.APPID;
-
   }
 
   function getData(callback){
     options.path = buildPath();
-    var req = http.get(options, function(res){
+    http.get(options, function(res){
       res.setEncoding('utf-8');
       res.on('data', function (chunk) {
-
           return callback(null, JSON.parse(chunk));
       });
 
       res.on('error', function(err){
           return callback(err, null);
-      })
+      });
     });
   }
-
-
 
 })();
